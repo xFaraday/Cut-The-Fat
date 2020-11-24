56cb2dc764443491eeacc4c510bbe4ecdf983548
xfaraday <theonlyfaraday@gmail.com>
2020-11-24 03:21:42 -0800

    test 1

--- cut-the-fat.ps1
+++ cut-the-fat.ps1
@@ -19,13 +19,16 @@ Param(
 )
 
 function startup {
-$startupnamearr @{
-                    #Names of bad startup programs
-}
-foreach ($startupname in $Starupnamearr){
-    Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run -Name $startupname -Value ([byte[]](0x33,0x32,0xFF))
+    #$startupnames = (gcim win32_startupcommand).Name 
+    $startups = gcim win32_startupcommand
+    #White list of good startup names
+    [regex]$WhiteList = 'SecurityHealth | (Default) | VMWare User Process'
+    foreach ($startup in $startups) {
+    Get-ItemProperty -Path ($startup).Location -Name ($startup).Name | Where-Object {$_.Name -NotMatch $WhiteList} | Set-ItemProperty -Value ([byte[]](0xEF,0xBE,0xAD,0xDE))
     }
+
 }
+
 
 function serv {
     $services = @(
@@ -50,7 +53,7 @@ function serv {
     
     foreach ($service in $services) {
         Stop-Service -Name $service -Force 
-        Set-Service -Name $service -Force
+        Set-Service -Name $service -StartupType Disabled -Force
     }
 
 }
@@ -80,10 +83,13 @@ function cortanatrack {
     #disable advertising info
     reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo" /v Enabled /t REG_DWORD /d 0 /f
 
+    #prevent bloatware from coming back from the dead
+    Set-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent" DisableWindowsConsumerFeatures -Value 1
+
 }
 
 function update {
-    #might have to use red add instead
+    #might have to use reg add instead
     set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" AUOptions -Value 1
     set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows" WindowsUpdate
     set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" AU
@@ -92,9 +98,6 @@ function update {
     set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" AUOptions -Value 2 #Hex Dword value of 2
     #reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v AUOptions /t REG_DWORD /d 2 /f
     reg ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v AutoInstallMinorUpdates /t REG_DWORD /d 0 /f
-
-
-
 
 }
 
@@ -121,6 +124,9 @@ function apps {
 
 
         "*Microsoft.BingNews*"
+        "*Microsoft.BingWeather*"
+        "*Microsoft.BingSports*"
+        "*Microsoft.BingFinance*"
         "*Microsoft.GetHelp*"
         "*Microsoft.Getstarted*"
         "*Microsoft.Messaging*"
@@ -146,10 +152,66 @@ function apps {
         "*Microsoft.XboxSpeechToTextOverlay*"
         "*Microsoft.ZuneMusic*"
         "*Microsoft.ZuneVideo*"
+        "*Microsoft.YourPhone*"
     )
+    foreach ($App in $Apps) {
+    Get-AppxPackage -Name $App | Remove-AppxPackage -ErrorAction SilentlyContinue
+    Get-AppxPackage -Name $App -AllUsers | Remove-AppxPackage -AllUsers -ErrorAction SilentlyContinue
+    Get-AppxPriovisionedPackage -Online | Where-Object DisplayName -like $App | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue
+    }
 
 }
 
+function tasks {
+    $tasks = @(
+        "XblGameSaveTaskLogon"
+        "XblGameSaveTask"               #xbox garbage
+        "Consolidator"                  #customer improvement program
+        "UsbCeip"                       #windows improvement program
+        "DmClient"                      #Microsoftfeedback
+        "DmClientOnScenarioDownload"    #Microsoftfeedback
+        "Appraiser"                     #Telemetry
+        "ProgramDataUpdater"            #Telemetry
+    )
+    foreach ($task in $tasks) {
+    Get-ScheduledTask $task | Disable-ScheduledTask
+    }
+}
+
+function onedrive {
+#killing onedrive
+taskkill /f /im OneDrive.exe
+& "$env:SystemRoot\SysWOW64\OneDriveSetup.exe" /uninstall
+
+#ownership of onedrivesetup.exe
+$ACL = Get-ACL -Path $env:SystemRoot\SysWOW64\OneDriveSetup.exe
+$Group = New-Object System.Security.Principal.NTAccount("$env:UserName")
+$ACL.SetOwner($Group)
+Set-Acl -Path $env:SystemRoot\SysWOW64\OneDriveSetup.exe -AclObject $ACL
+
+#Assign full R/W Permissions to $env:UserName (Administrator)
+$Acl = Get-Acl "$env:SystemRoot\SysWOW64\OneDriveSetup.exe"
+$Ar = New-Object  system.security.accesscontrol.filesystemaccessrule("$env:UserName","FullControl","Allow")
+$Acl.SetAccessRule($Ar)
+Set-Acl "$env:SystemRoot\SysWOW64\OneDriveSetup.exe" $Acl
+    
+REG Delete "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f
+REG Delete "HKEY_CLASSES_ROOT\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f
+
+#Remove one drive shortcuts
+Remove-Item -Path "$env:SystemRoot\SysWOW64\OneDriveSetup.exe" -Force -ErrorAction SilentlyContinue
+
+Remove-Item -Path "$env:SystemRoot\SysWOW64\OneDrive.ico" -Force -ErrorAction SilentlyContinue
+
+Remove-Item -Path "$env:USERPROFILE\OneDrive" -Recurse -Force -ErrorAction SilentlyContinue
+
+Remove-Item -Path "$env:LOCALAPPDATA\Microsoft\OneDrive" -Recurse -Force -ErrorAction SilentlyContinue
+
+Remove-Item -Path "$env:ProgramData\Microsoft OneDrive" -Recurse -Force -ErrorAction SilentlyContinue
+
+Remove-Item -Path "C:\OneDriveTemp" -Recurse -Force -ErrorAction SilentlyContinue
+
+}
 
 if ($All) {
     startup
@@ -159,8 +221,10 @@ if ($All) {
     update
     windef
     apps
+    tasks
+    onedrive
 } else {
-
+    Write-Warning "No Parameter"
 }
 
 }
