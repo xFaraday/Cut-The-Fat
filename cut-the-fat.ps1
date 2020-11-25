41c8bfdb143aff5a50b69f051a1ddb00f824ba26
xfaraday <theonlyfaraday@gmail.com>
2020-11-25 10:36:37 -0800

    2nd

--- cut-the-fat.ps1
+++ cut-the-fat.ps1
@@ -9,12 +9,24 @@ bloatware is to reduce CPU usage at start up and idle CPU usage.
 
 Run every script
 
+.PARAMETER Win10
+
+Specifies to be run on windows 10
+
+.PAREMETER Serv
+
+Specifies to be run on Windows Server
+
 #>
 
 [CmdletBinding(DefaultParameterSetName="default")]
 Param(
     [Parameter(ParameterSetName='All')]
-    [switch]$All
+    [switch]$All,
+    [Parameter(ParameterSetName='Win10')]
+    [switch]$Win10,
+    [Parameter(ParameterSetName='Serv')]
+    [switch]$Serv
 
 )
 
@@ -26,7 +38,6 @@ function startup {
     foreach ($startup in $startups) {
     Get-ItemProperty -Path ($startup).Location -Name ($startup).Name | Where-Object {$_.Name -NotMatch $WhiteList} | Set-ItemProperty -Value ([byte[]](0xEF,0xBE,0xAD,0xDE))
     }
-
 }
 
 
@@ -60,17 +71,34 @@ function serv {
 }
 
 function visualreg {
-  #Doesnt look like there is an easy way to set this automatically...will come back to this later
+    reg ADD "" /v /t REG_DWORD /d /f
 
+    #live tiles
+    reg ADD "HKCU\Software\Policies\Microsoft\Windows\CurrentVersion\PushNotifications" /v NoTileApplicationsNotification /t REG_DWORD /d 1 /f
+    #People in taskbar
+    reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People" /v PeopleBand /t REG_DWORD /d 0 /f
+    #suggestions start menu
+    reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SystemPaneSuggestionsEnabled /t REG_DWORD /d 0 /f
+    
+    #Keys that add the desktop and how it handles tasks related to it 
+    reg ADD "HKCU\Control Panel\Desktop" /v MenuShowDelay /t REG_SZ /d 75 /f
+    reg ADD "HKCU\Control Panel\Desktop" /v AutoEndTasks /t REG_SZ /d 1 /f
+    
+    #APPLYING VISUAL SETTINGS FOR BEST PERFORMANCE...finally found the key
+    reg ADD "HKCU\Control Panel\Desktop" /v UserPreferencesMask /t REG_BINARY /d "90 12 03 80 10 00 00 00" /f
 }
 
-function cortanatrack {
+function trackwack {
     #disable cortana
-    Set-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" AllowCortana -Value 0
+    reg ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v AllowCortana /t REG_DWORD /d 0 /f
 
     #Disable telemetry
-    Set-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" Allow Telemetry -Value 0
-    Set-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Policies\DataCollection" Allow Telemetry -Value 0
+    #Set-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" Allow Telemetry -Value 0
+    reg ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v "Allow Telemetry" /t REG_DWORD /d 0 /f
+    #Set-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Policies\DataCollection" Allow Telemetry -Value 0
+    reg ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Policies\DataCollection" /v "Allow Telemetry" /t REG_DWORD /d 0 /f 
+
+    #telemetry tasks
     schtasks /Change /TN "Microsoft\Office\OfficeTelemetryAgentLogOn" /Disable
     schtasks /Change /TN "Microsoft\Office\OfficeTelemetryAgentFallBack" /Disable
     schtasks /Change /TN "Microsoft\Office\Office 15 Subscription Heartbeat" /Disable
@@ -82,24 +110,22 @@ function cortanatrack {
     reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v WarnonZoneCrossing /t REG_DWORD /d 1 /f
 
     #disable advertising info
-    reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo" /v Enabled /t REG_DWORD /d 0 /f
+    reg ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo" /v Enabled /t REG_DWORD /d 0 /f
 
     #prevent bloatware from coming back from the dead
-    Set-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent" DisableWindowsConsumerFeatures -Value 1
+    #Set-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent" DisableWindowsConsumerFeatures -Value 1
+    reg ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /v DisableWindowsConsumerFeatures /t REG_DWORD /d 1 /f
 
 }
 
 function update {
-    #might have to use reg add instead
-    set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" AUOptions -Value 1
-    set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows" WindowsUpdate
-    set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" AU
-    set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" NoAutoUpdate -Value 1
-    reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v NoAutoUpdate /t REG_DWORD /d 1 /f
-    set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" AUOptions -Value 2 #Hex Dword value of 2
-    reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v AUOptions /t REG_DWORD /d 2 /f
+    #set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" AUOptions -Value 1
+    reg ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v "Auto Update" /t REG_DWORD /d 1 /f
+    #set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" NoAutoUpdate -Value 1
+    reg ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v NoAutoUpdate /t REG_DWORD /d 1 /f
+    #set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" AUOptions -Value 2 #Hex Dword value of 2
+    reg ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v AUOptions /t REG_DWORD /d 2 /f
     reg ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v AutoInstallMinorUpdates /t REG_DWORD /d 0 /f
-
 }
 
 function windef {
@@ -107,6 +133,8 @@ function windef {
     Set-MpPreference -ScanAvgCPULoadFactor 15
     #low pririoty process
     set-MpPreference -EnableLowCpuPriority $True 
+    #real time monitoring
+    Set-MpPreference -DisableRealTimeMonitoring $True
 }
 
 function apps {
@@ -179,55 +207,111 @@ function tasks {
     }
 }
 
+#simon lee one drive script
 function onedrive {
-#killing onedrive
-taskkill /f /im OneDrive.exe
-& "$env:SystemRoot\SysWOW64\OneDriveSetup.exe" /uninstall
+    #killing onedrive
+    taskkill /f /im OneDrive.exe
+    & "$env:SystemRoot\SysWOW64\OneDriveSetup.exe" /uninstall
 
-#ownership of onedrivesetup.exe
-$ACL = Get-ACL -Path $env:SystemRoot\SysWOW64\OneDriveSetup.exe
-$Group = New-Object System.Security.Principal.NTAccount("$env:UserName")
-$ACL.SetOwner($Group)
-Set-Acl -Path $env:SystemRoot\SysWOW64\OneDriveSetup.exe -AclObject $ACL
+    #ownership of onedrivesetup.exe
+    $ACL = Get-ACL -Path $env:SystemRoot\SysWOW64\OneDriveSetup.exe
+    $Group = New-Object System.Security.Principal.NTAccount("$env:UserName")
+    $ACL.SetOwner($Group)
+    Set-Acl -Path $env:SystemRoot\SysWOW64\OneDriveSetup.exe -AclObject $ACL
 
-#Assign full R/W Permissions to $env:UserName (Administrator)
-$Acl = Get-Acl "$env:SystemRoot\SysWOW64\OneDriveSetup.exe"
-$Ar = New-Object  system.security.accesscontrol.filesystemaccessrule("$env:UserName","FullControl","Allow")
-$Acl.SetAccessRule($Ar)
-Set-Acl "$env:SystemRoot\SysWOW64\OneDriveSetup.exe" $Acl
-    
-REG Delete "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f
-REG Delete "HKEY_CLASSES_ROOT\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f
+    #Assign full R/W Permissions to $env:UserName (Administrator)
+    $Acl = Get-Acl "$env:SystemRoot\SysWOW64\OneDriveSetup.exe"
+    $Ar = New-Object  system.security.accesscontrol.filesystemaccessrule("$env:UserName","FullControl","Allow")
+    $Acl.SetAccessRule($Ar)
+    Set-Acl "$env:SystemRoot\SysWOW64\OneDriveSetup.exe" $Acl
+        
+    REG Delete "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f
+    REG Delete "HKEY_CLASSES_ROOT\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f
 
-#Remove one drive shortcuts
-Remove-Item -Path "$env:SystemRoot\SysWOW64\OneDriveSetup.exe" -Force -ErrorAction SilentlyContinue
+    #Remove one drive shortcuts
+    Remove-Item -Path "$env:SystemRoot\SysWOW64\OneDriveSetup.exe" -Force -ErrorAction SilentlyContinue
 
-Remove-Item -Path "$env:SystemRoot\SysWOW64\OneDrive.ico" -Force -ErrorAction SilentlyContinue
+    Remove-Item -Path "$env:SystemRoot\SysWOW64\OneDrive.ico" -Force -ErrorAction SilentlyContinue
 
-Remove-Item -Path "$env:USERPROFILE\OneDrive" -Recurse -Force -ErrorAction SilentlyContinue
+    Remove-Item -Path "$env:USERPROFILE\OneDrive" -Recurse -Force -ErrorAction SilentlyContinue
 
-Remove-Item -Path "$env:LOCALAPPDATA\Microsoft\OneDrive" -Recurse -Force -ErrorAction SilentlyContinue
+    Remove-Item -Path "$env:LOCALAPPDATA\Microsoft\OneDrive" -Recurse -Force -ErrorAction SilentlyContinue
 
-Remove-Item -Path "$env:ProgramData\Microsoft OneDrive" -Recurse -Force -ErrorAction SilentlyContinue
+    Remove-Item -Path "$env:ProgramData\Microsoft OneDrive" -Recurse -Force -ErrorAction SilentlyContinue
 
-Remove-Item -Path "C:\OneDriveTemp" -Recurse -Force -ErrorAction SilentlyContinue
+    Remove-Item -Path "C:\OneDriveTemp" -Recurse -Force -ErrorAction SilentlyContinue
 
 }
 
-if ($All) {
+#credit to Alex for this part
+function keyterm {
+    $Keys = @(
+        
+    #Remove Background Tasks
+    "HKCR:\Extensions\ContractId\Windows.BackgroundTasks\PackageId\46928bounde.EclipseManager_2.2.4.51_neutral__a5h4egax66k6y"
+    "HKCR:\Extensions\ContractId\Windows.BackgroundTasks\PackageId\ActiproSoftwareLLC.562882FEEB491_2.6.18.18_neutral__24pqs290vpjk0"
+    "HKCR:\Extensions\ContractId\Windows.BackgroundTasks\PackageId\Microsoft.MicrosoftOfficeHub_17.7909.7600.0_x64__8wekyb3d8bbwe"
+    "HKCR:\Extensions\ContractId\Windows.BackgroundTasks\PackageId\Microsoft.PPIProjection_10.0.15063.0_neutral_neutral_cw5n1h2txyewy"
+    "HKCR:\Extensions\ContractId\Windows.BackgroundTasks\PackageId\Microsoft.XboxGameCallableUI_1000.15063.0.0_neutral_neutral_cw5n1h2txyewy"
+    "HKCR:\Extensions\ContractId\Windows.BackgroundTasks\PackageId\Microsoft.XboxGameCallableUI_1000.16299.15.0_neutral_neutral_cw5n1h2txyewy"
+        
+    #Windows File
+    "HKCR:\Extensions\ContractId\Windows.File\PackageId\ActiproSoftwareLLC.562882FEEB491_2.6.18.18_neutral__24pqs290vpjk0"
+        
+    #Registry keys to delete if they aren't uninstalled by RemoveAppXPackage/RemoveAppXProvisionedPackage
+    "HKCR:\Extensions\ContractId\Windows.Launch\PackageId\46928bounde.EclipseManager_2.2.4.51_neutral__a5h4egax66k6y"
+    "HKCR:\Extensions\ContractId\Windows.Launch\PackageId\ActiproSoftwareLLC.562882FEEB491_2.6.18.18_neutral__24pqs290vpjk0"
+    "HKCR:\Extensions\ContractId\Windows.Launch\PackageId\Microsoft.PPIProjection_10.0.15063.0_neutral_neutral_cw5n1h2txyewy"
+    "HKCR:\Extensions\ContractId\Windows.Launch\PackageId\Microsoft.XboxGameCallableUI_1000.15063.0.0_neutral_neutral_cw5n1h2txyewy"
+    "HKCR:\Extensions\ContractId\Windows.Launch\PackageId\Microsoft.XboxGameCallableUI_1000.16299.15.0_neutral_neutral_cw5n1h2txyewy"
+        
+    #Scheduled Tasks to delete
+    "HKCR:\Extensions\ContractId\Windows.PreInstalledConfigTask\PackageId\Microsoft.MicrosoftOfficeHub_17.7909.7600.0_x64__8wekyb3d8bbwe"
+        
+    #Windows Protocol Keys
+    "HKCR:\Extensions\ContractId\Windows.Protocol\PackageId\ActiproSoftwareLLC.562882FEEB491_2.6.18.18_neutral__24pqs290vpjk0"
+    "HKCR:\Extensions\ContractId\Windows.Protocol\PackageId\Microsoft.PPIProjection_10.0.15063.0_neutral_neutral_cw5n1h2txyewy"
+    "HKCR:\Extensions\ContractId\Windows.Protocol\PackageId\Microsoft.XboxGameCallableUI_1000.15063.0.0_neutral_neutral_cw5n1h2txyewy"
+    "HKCR:\Extensions\ContractId\Windows.Protocol\PackageId\Microsoft.XboxGameCallableUI_1000.16299.15.0_neutral_neutral_cw5n1h2txyewy"
+           
+    #Windows Share Target
+    "HKCR:\Extensions\ContractId\Windows.ShareTarget\PackageId\ActiproSoftwareLLC.562882FEEB491_2.6.18.18_neutral__24pqs290vpjk0"
+    )
+    
+    ForEach ($Key in $Keys) {
+        Write-Output "Removing $Key from registry"
+        Remove-Item $Key -Recurse -ErrorAction SilentlyContinue
+    }
+
+}
+
+if ($All -and $Win10) {
     startup
     serv
     visualreg
-    cortanatrack
+    trackwack
     update
     windef
     apps
     tasks
     onedrive
+    keyterm
+} elseif ($All -and $Serv) {
+    startup
+    serv
+    visualreg
+    trackwack
+    update
+    windef
+    apps
+    tasks
+    onedrive
+    keyterm
 } else {
-    Write-Warning "No Parameter"
+    Write-Warning "Check Parameters"
 }
 
 }
 
-Invoke-FatBurn -All
+#Invoke-FatBurn -All -Win10
+#Invoke-FatBurn -All -Serv
